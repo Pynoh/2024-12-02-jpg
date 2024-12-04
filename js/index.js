c94315c62bd44351b866b1fe85029cdddbd8ed3b
Pynoh <ol.mahieu@gmail.com>
2024-12-04 10:45:42 +0100

    modification de la barre jsnotif

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
 if(confirm("Le fichier js est chargé")) {
     console.log("C'est okaaaay");
+    var jsnotif = document.querySelector('#js-notification');
+    jsnotif.style.backgroundColor = 'green';
+    jsnotif.innerHTML = "JS OK ! \\o/" ;
 }
