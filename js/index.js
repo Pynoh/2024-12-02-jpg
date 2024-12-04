6bcf233cc6468226f8b263ce2e28c2d48d539f5d
Pynoh <ol.mahieu@gmail.com>
2024-12-04 12:33:28 +0100

    Tests sur les boutons, addEventListener

--- js/index.js
+++ js/index.js
@@ -1,6 +1,20 @@
-if(confirm("Le fichier js est chargé")) {
-    console.log("C'est okaaaay");
-    var jsnotif = document.querySelector('#js-notification');
-    jsnotif.style.backgroundColor = 'green';
-    jsnotif.innerHTML = "JS OK ! \\o/" ;
+function loadingDOM() {
+  console.log("C'est okaaaay");
+  var jsnotif = document.querySelector("#js-notification");
+  jsnotif.style.backgroundColor = "green";
+  jsnotif.innerHTML = 'JS OK ! <span id="victory-man">\\o/</span>';
+  var victoryMan = jsnotif.children[0];
+  victoryMan.style.fontStyle = "italic";
+  victoryMan.style.color = "red";
+  var buttonHeader = document.querySelector("#header-button");
+  buttonHeader.addEventListener("click", function (evt) {
+    evt.stopPropagation();
+    console.log(evt);
+    console.log("Au secours");
+  });
+  document.querySelector("#header").addEventListener("click", function (evt) {
+    console.log(evt);
+    console.log("tout va bien");
+  });
 }
+document.addEventListener("DOMContentLoaded", loadingDOM);
