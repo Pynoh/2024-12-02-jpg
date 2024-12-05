ece6192a4e431d22da390bf9eb0e302ead9d2520
Pynoh <ol.mahieu@gmail.com>
2024-12-05 14:51:14 +0100

    création de la classe Meme et du db.json

---
+++ js/metier/meme.js
@@ -0,0 +1,28 @@
+class Meme {
+  id = undefined;
+  text = "";
+  x = 0;
+  y = 10;
+  fontSize = 10;
+  fontWeight = 500;
+  underline = false;
+  italic = false;
+  color = "#FFFFFF";
+  imageId = -1;
+  #endpoint = "/memes";
+
+  constructor() {}
+  save() {
+    console.log("save " + this.id + " at " + this.#endpoint, this);
+    this.publicSave();
+    this.#privateSave();
+  }
+  publicSave() {
+    console.log("public saving");
+  }
+  #privateSave() {
+    console.log("private saving");
+  }
+}
+let meme = new Meme();
+meme.save();
